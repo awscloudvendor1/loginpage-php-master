48424a0160f3d99b61362d9b36903dc0d5fcb827
awscloudvendor1 <83281099+awscloudvendor1@users.noreply.github.com>
2022-07-13 06:17:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -105,10 +105,10 @@
             ?>" method = "post">
             <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
             <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username = whatname" 
+               name = "username" placeholder = "username = TypetTheName" 
                required autofocus></br>
             <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = Credentials" required>
+               name = "password" placeholder = "password = TypeAuth" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
                name = "login">Login</button>
          </form>
