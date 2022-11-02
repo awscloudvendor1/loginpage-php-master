a4a5967eb09e4438a937b298a10c89b775ffad33
awscloudvendor1 <83281099+awscloudvendor1@users.noreply.github.com>
2022-11-02 07:37:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
                name = "username" placeholder = "username = Identityplz" 
                required autofocus></br>
             <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = AuthHere" required>
+               name = "password" placeholder = "password = Authentication" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
                name = "login">Login</button>
          </form>
